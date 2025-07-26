f192d2f3767603c718d5a0faf2ba769e0283880c
danushkacodeolima <danushka@codeolima.com>
2025-07-26 06:18:12 +0530

    comment

--- src/application/services/SeatChangeQueueManager.java
+++ src/application/services/SeatChangeQueueManager.java
@@ -18,6 +18,8 @@ public class SeatChangeQueueManager {
 
         System.out.println("Customer " + reservation.getCustomer().getName()
                 + " added to waiting list for seat " + desiredSeatNumber + " on bus " + busNumber);
+        
+        System.out.println("k");
     }
 
     // Process seat availability (called after a seat becomes free)
